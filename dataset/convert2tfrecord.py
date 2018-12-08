051814a0eaca3a397156e04374c1a26434f8ab7e
funkyfox <326781458@qq.com>
2018-12-08 13:10:31 +0800

    add preprocess in convert2tfrecord.py

--- dataset/convert2tfrecord.py
+++ dataset/convert2tfrecord.py
@@ -14,6 +14,7 @@ LABELS_FILENAME = 'labels.txt'
 # The shards of output files
 _NUM_SHARDS = 5
 
+SEED = 7
 # The height and width of each image.
 _IMAGE_SIZE = 32
 
@@ -38,6 +39,59 @@ _CLASS_NAMES = [
     'Water',
 ]
 
+# shuffle
+_NEW_ORDER = list(range(352366))
+np.random.seed(SEED)
+np.random.shuffle(_NEW_ORDER)
+_SHUFFLE_MAP = {i:j for i,j in zip(list(range(352366)), _NEW_ORDER)}
+
+S1_THRESHS = [(-25, 25), (-25, 25), (-70, 70), (-70, 70), (float("-inf"), 100), (float("-inf"), 1000),
+              (-100, 100), (-100, 100)]  # 上界，下界
+S2_THRESHS = [(float("-inf"), float("inf")), (float("-inf"), float("inf")), (float("-inf"), float("inf")),
+              (float("-inf"), float("inf")), (float("-inf"), float("inf")), (float("-inf"), float("inf")),
+              (float("-inf"), float("inf")), (float("-inf"), float("inf")), (float("-inf"), float("inf")),
+              (float("-inf"), float("inf"))]
+S1_MEANS_STDS = [(-3.1764201098502775e-05, 0.17253755233246393), (-7.0324133129218994e-06, 0.17233924335556297), (6.4947807422303307e-05, 0.45648411453353621), (2.9759795273244554e-05, 0.45325171128651598), (0.04255387426528303, 0.35996098865778275), (0.24984416768306306, 3.9778058737510902), (0.00076266614542442929, 0.42671998442466774), (0.00090506412854692508, 0.31763204200829365)]
+S2_MEANS_STDS = [(0.1237569611768191, 0.03958795985905441), (0.1092774636368305, 0.047778262752410657), (0.10108552032678805, 0.066366167063719519), (0.11423986161140066, 0.063588749124974239), (0.15926566920230753, 0.07744387147984555), (0.18147236008771511, 0.091016350859215042), (0.1745740312291362, 0.092184665623869899), (0.19501607349634489, 0.1016458123394836), (0.15428468872076573, 0.099917730435192545), (0.10905050699570018, 0.087806325091228543)]
+
+
+def preprocess(data, sen):
+    import copy
+    data = copy.deepcopy(data)
+    if sen == "sen1":
+        t = S1_THRESHS
+        ms = S1_MEANS_STDS
+    else:
+        t = S2_THRESHS
+        ms = S2_MEANS_STDS
+
+    # 去异常
+    lo_threshs = np.array([_[0] for _ in t])
+    bool_matrix = data < lo_threshs.reshape(1, 1, -1)
+    data = np.where(bool_matrix, np.tile(lo_threshs, (_IMAGE_SIZE, _IMAGE_SIZE, 1)), data)
+
+    hi_threshs = np.array([_[1] for _ in t])
+    bool_matrix = data > hi_threshs.reshape(1, 1, -1)
+    data = np.where(bool_matrix, np.tile(hi_threshs, (_IMAGE_SIZE, _IMAGE_SIZE, 1)), data)
+
+    # 标准化
+    means = np.array([_[0] for _ in ms]).reshape(1, 1, -1)
+    stds = np.array([_[1] for _ in ms]).reshape(1, 1, -1)
+    data = (data - means) / stds
+
+    return data
+
+def sen2_preprocess(data):
+    import copy
+    data = copy.deepcopy(data)
+    chan_num = data.shape[-1]
+    for i in range(chan_num):
+        lo_thresh, hi_thresh = S2_THRESHS[i]
+        mean, std = S2_MEANS_STDS[i]
+        data[:, :, i][data[:,:,i] > hi_thresh] = hi_thresh
+        data[:, :, i][data[:,:,i] < lo_thresh] = hi_thresh
+        data[:, :, i] = (data[:,:,i] - mean) / std
+    return data
 
 def float_feature(value):
     return tf.train.Feature(float_list=tf.train.FloatList(value=value))
@@ -85,18 +139,21 @@ def _add_to_tfrecord(filename, num_shards, tfrecord_filename, split_name, offset
             with tf.python_io.TFRecordWriter(
                     tfrecord_filename % ('0' * (4 - len(str(shard_id))) + str(shard_id))) as tfrecord_writer:
                 for j in range(start_ndx, end_ndx):
+                    if split_name == 'train':
+                        j = _SHUFFLE_MAP[j]
                     sys.stdout.write('\r>> Reading file [%s] image %d/%d' % (
                         filename, offset + j + 1, offset + num_images))  # \r：光标移到本行最左边
                     sys.stdout.flush()
+
                     if split_name == 'train' or split_name == 'validation':
-                        example = to_tfexample(sen1[j].reshape(-1),
-                                               sen2[j].reshape(-1),
+                        example = to_tfexample(preprocess(sen1[j], "sen1").reshape(-1),
+                                               preprocess(sen2[j], "sen2").reshape(-1),
                                                _IMAGE_SIZE,
                                                split_name,
                                                np.argmax(labels[j]))  # reshape(-1): 相当于ravel()
                     else:
-                        example = to_tfexample(sen1[j].reshape(-1),
-                                               sen2[j].reshape(-1),
+                        example = to_tfexample(preprocess(sen1[j], "sen1").reshape(-1),
+                                               preprocess(sen2[j], "sen2").reshape(-1),
                                                _IMAGE_SIZE,
                                                split_name)
                     tfrecord_writer.write(example.SerializeToString())
